ebe5089664eb2380e237bf4d1feeb1c365644d9d
Oussama <oussamacherifi7@gmail.com>
2023-02-27 14:38:58 -0500

    adding posts and databae connection

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -229,41 +229,6 @@ function DashboardContent() {
             </table>
           </Container>
 
-
-          {/* <Container sx={{ py: 8 }} maxWidth="md">
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random"
-                    alt="random"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      hello you
-
-                    </Typography>
-                    <Typography>
-                      hello sir
-
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Apply</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-
-          </Container> */}
         </Box>
       </Box>
     </ThemeProvider>
