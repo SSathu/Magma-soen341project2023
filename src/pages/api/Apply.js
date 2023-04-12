4fc39a06b8267f2795304e87a5cbf80392333383
Oussama <oussamacherifi7@gmail.com>
2023-04-12 15:40:21 -0400

    implementing apply feature closes#68

---
+++ src/pages/api/Apply.js
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+    
+  console.log("called handler")  
+  const {email, jobid, Viewed, Accepted } = req.body
+
+  try {
+    const Applications = await prisma.Applications.create({
+      data: {
+        Email : email,
+        JobId : jobid,
+        Viewed: Viewed,
+        Accepted : Accepted
+      }
+    })
+
+    res.status(200).json({ message: 'Application submitted successfully' })
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to submit application' })
+  }
+}
